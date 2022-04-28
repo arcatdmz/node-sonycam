72e3afde11ea7c65454953343a8f18fd9f669394
Jun Kato <i@junkato.jp>
2022-04-29 06:36:54 +0900

    Bundle ESM only

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,17 @@
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json";
 
 export default [
   {
     input: "lib/index.ts",
-    output: [
-      {
-        file: pkg.main,
-        format: "cjs",
-      },
-      {
-        file: pkg.module,
-        format: "es",
-      },
-    ],
+    output: {
+      file: pkg.module,
+      format: "es",
+    },
     external: [
       ...Object.keys(pkg.dependencies || {}),
       ...Object.keys(pkg.peerDependencies || {}),
     ],
-    plugins: [
-      typescript({
-        typescript: require("typescript"),
-        tsconfig: "tsconfig.json",
-      }),
-    ],
+    plugins: [typescript({ tsconfig: "./tsconfig.json" })],
   },
 ];
